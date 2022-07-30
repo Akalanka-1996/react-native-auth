f779785203aef94ad37252e36f53adb93f7de0ca
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-30 13:53:05 +0530

    register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import React, {useState} from "react";
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+      <TextInput 
+          style={styles.input} 
+          value={name}
+          placeholder="Enter Name" 
+          onChangeText={text => setName(text)}
+        />
+        <TextInput 
+          style={styles.input} 
+          value={email}
+          placeholder="Enter Email" 
+          onChangeText={text => setEmail(text)}
+        />
+        <TextInput 
+          style={styles.input} 
+          value={password}
+          placeholder="Enter Password" 
+          secureTextEntry 
+          onChangeText={text => setPassword(text)}
+        />
+        <Button title="Login" />
 
-export default RegisterScreen
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <Text>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.link}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrapper: {
+    width: "80%",
+  },
+  input: {
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: '#bbb',
+    borderRadius: 5,
+    paddingHorizontal: 14
+  },
+  link: {
+    color: 'blue'
+  }
+});
+
+export default RegisterScreen;
